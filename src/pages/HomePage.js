cefabb70bfe68cec5cee9680d53d91bae682eb29
devosc2 <oscar.anguiano@nisamerica.com>
2021-10-22 00:48:53 -0600

    mvp

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -24,47 +24,9 @@ export const HomePage = ({ setModal }) => {
                 func={() => console.log("Resume")}
               />
             </a>
-
-            <Btn
-              icon="email"
-              title={"Hire Me"}
-              func={() => setModal(<Contact />)}
-            />
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-const Contact = () => {
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [message, setMessage] = React.useState("");
-  return (
-    <div id="contact" className="flex">
-      <input
-        type="text"
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.currentTarget.value)}
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.currentTarget.value)}
-      />
-      <textarea
-        placeholder="Message"
-        name=""
-        id=""
-        cols="30"
-        rows="10"
-        value={message}
-        onChange={(e) => setMessage(e.currentTarget.value)}
-      ></textarea>
-      <Btn title="send" variant="secondary" />
     </div>
   );
 };
